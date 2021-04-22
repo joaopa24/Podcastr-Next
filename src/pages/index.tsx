48c7650b89f8fd51908d4c2f754ad1512aa0ce05
joaopa24 <joaokn129@gmail.com>
2021-04-22 00:13:39 -0300

    API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import { Header } from "../components/Header";
+// SPA 
+// SSR
+// SSG
 
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes').then(response => response.json()).then(data => console.log(data))
+  }, [])
   return (
     <h1>Index</h1>
   )
