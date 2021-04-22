e31123a9bb8f5b662a67b063bfefae29c8c74736
joaopa24 <joaokn129@gmail.com>
2021-04-21 23:18:41 -0300

    currentDate in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,11 @@
 import styles from './styles.module.scss'
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
 
 export function Header(){
-   const currentDate = new Date().toLocaleDateString()
+   const currentDate = format(new Date(), 'EEEEEE, d MMMM',{
+       locale: ptBR,
+   });
 
    return(
        <header className={styles.headerContainer}>
@@ -9,7 +13,7 @@ export function Header(){
 
            <p>O melhor para você ouvir, sempre</p>
 
-           <span>Qui, 8 Abril</span>
+           <span>{currentDate}</span>
        </header>
    )
 }
