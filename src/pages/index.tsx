1467e39a6d4b57fe193d1503cabd98779ee6a91d
joaopa24 <joaokn129@gmail.com>
2022-01-05 00:36:37 -0300

    page of episode scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,6 @@ type Episode = {
   id: string;
   title: string;
   thumbnail: string;
-  description: string;
   duration: number;
   durationAsString: number;
   members: string;
@@ -44,7 +43,9 @@ export default function Home({ latestEpisodes, allEpisodes}: HomeProps) {
                    />
 
                    <div className={styles.episodeDetails}>
-                      <a href={`/episodes/${episode.id}`}>{episode.title}</a>
+                     <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                      </Link>
                       <p>{episode.members}</p>
                       <span>{episode.publishedAt}</span>
                       <span>{episode.durationAsString}</span>
@@ -64,12 +65,14 @@ export default function Home({ latestEpisodes, allEpisodes}: HomeProps) {
 
            <table cellSpacing={0}>
              <thead>
+                <tr>
                 <th></th>
                 <th>Podcast</th>
                 <th>Integrantes</th>
                 <th>Data</th>
                 <th>Duração</th>
                 <th></th>
+                </tr>
              </thead>
              <tbody>
                {allEpisodes.map(episode =>{
@@ -84,7 +87,9 @@ export default function Home({ latestEpisodes, allEpisodes}: HomeProps) {
                         /> 
                       </td>
                       <td>
-                        <a href="">{episode.title}</a>
+                        <Link href={`/episodes/${episode.id}`}>
+                        <a>{episode.title}</a>
+                        </Link>
                       </td>
                       <td>{episode.members}</td>
                       <td style={{width:100 }}>{episode.publishedAt}</td>
@@ -124,7 +129,6 @@ export const getStaticProps: GetStaticProps = async () => {
         publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
         duration: Number(episode.file.duration),
         durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-        description: episode.description,
         url: episode.file.url
       };
     })
