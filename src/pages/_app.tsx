e1bf320fc500cabe81e40ea8409e2829af9b5028
joaopa24 <joaokn129@gmail.com>
2022-01-22 01:20:48 -0300

    play title episodes working!

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,19 @@ import '../styles/global.scss'
 import { Header } from '../components/Header'
 import { Player } from '../components/Player'
 import { PlayerContext } from '../contexts/PlayerContext'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+
+  function play(episode){
+    setEpisodeList([episode])
+    setCurrentEpisodeIndex(0)
+  }
+
   return (
-    <PlayerContext.Provider value={'Joao'}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
       <div className={styles.wrapper}>
       <main>
           <Header />
